09268ab0682fa186d28f673ae97eaf4cd999cd0d
folkforms <alan.bergin@gmail.com>
2023-08-02 18:56:34 +0100

    Line now moves along with the circle

--- src/d3/Test1.jsx
+++ src/d3/Test1.jsx
@@ -1,51 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as d3 from "d3";
-// import { drag } from "d3";
 import "./d3.css";
-import { render } from '@testing-library/react';
 
-const flip = (data, height) => data.map(coord => [coord[0], height - coord[1]]);
+const Test1 = ({ width = 640, height = 400 }) => {
+  const [pos, setPos] = useState([100, 200]);
 
-const Test1 = ({
-  data,
-  width = 640,
-  height = 400,
-  marginTop = 20,
-  marginRight = 20,
-  marginBottom = 20,
-  marginLeft = 20
-}) => {
   const line = d3.line().curve(d3.curveCatmullRom.alpha(1));
-  let d = line([[0, 0], [100, 200], [400, 400]]);
+  let d = line([[0, 0], [pos[0], pos[1]], [400, 400]]);
 
-  const myReference = React.createRef();
-
-  const foo = () => {
+  const setUpDrag = () => {
     const handleDrag = d3.drag()
     .subject(function() {
-      console.log("subject");
       const me = d3.select(this);
       return { x: me.attr('cx'), y: me.attr('cy') }
     })
     .on('drag', function(event) {
-      console.log("drag");
       const me = d3.select(this);
       me.attr('cx', event.x);
       me.attr('cy', event.y);
-
-      d = line([[0, 0], [event.x, event.y], [400, 400]]);
+      setPos([event.x, event.y]);
     });
     const bar = d3.select("#middle-circle");
-    console.log(`bar = ${bar}`);
     handleDrag(bar);
   }
 
+  useEffect(() => setUpDrag(), []);
+
   return (
-    <div className="outline margin-all-10px">
-      <svg width={width} height={height} onClick={foo}>
+    <div className="outline margin-all-10px padding-all-10px">
+      <svg width={width} height={height}>
         <path fill="none" stroke="currentColor" strokeWidth="1.5" d={d} />
         <circle stroke="currentColor" key={"some key 1"} cx={0} cy={0} r="5" />
-        <circle id="middle-circle" stroke="currentColor" key={"some key 2"} cx={100} cy={200} r="5" />
+        <circle id="middle-circle" stroke="currentColor" key={"some key 2"} cx={pos[0]} cy={pos[1]} r="5" />
         <circle stroke="currentColor" key={"some key 3"} cx={400} cy={400} r="5" />
       </svg>
     </div>
