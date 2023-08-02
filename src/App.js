31f41fbf3b82d5574212f7571eafe4c9ef6ff24c
folkforms <alan.bergin@gmail.com>
2023-08-02 23:40:31 +0100

    Update copy

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const App = () => {
               <br />
               <Login2 />
               <div className="dark center padding-top-100px">
-                <div>Note: You will likely see a warning the app not being verified. This is because the verification process takes days to weeks.</div>
+                <div><strong>NB:</strong> You will likely see a warning about the app not being verified. This is because the verification process takes days to weeks.</div>
                 <div>Please click through the warnings as shown below. You can use a throwaway Gmail account if you are security conscious.</div>
               </div>
               <div className="center images-container">
